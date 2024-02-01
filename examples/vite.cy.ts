7b05f435bcf0a5088ebc4b59ca4b35fceff61f91
Josh McCall <joshmccall@microsoft.com>
2024-02-01 16:53:47 +0000

    example updates

--- examples/vite.cy.ts
+++ examples/vite.cy.ts
@@ -1,12 +1,51 @@
 
 describe('template spec', () => {
 
-  it('init', () => {
+  beforeEach(() => {
     cy.visit('http://127.0.0.1:5173/')
-    cy.contains('')
   })
-  it('init', () => {
+
+  it('init', () => { })
+
+  it('vite', () => {
+    cy.get('h1').contains('Vite')
+    cy.contains('Vite')
+  })
+
+  it('button', () => {
+    cy.contains('count is 0')
+    cy.get('button').click()
+    cy.contains('count is 1')
+  })
+})
+
+// //App.tsx
+// const [increment, setIncrement] = useState(1)
+
+// React.useEffect(() => {
+//   fetch('/getCountInterval.json')
+//     .then(results => results.json())
+//     .then(data => { setIncrement(data) })
+// }, [])
+
+// <button onClick={() => setCount((count) => count + increment)}>
+
+describe.only('network', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/getCountInterval.json', { body: Math.floor(Math.random() * 10) }).as('getCountInterval')
     cy.visit('http://127.0.0.1:5173/')
-    cy.get('h1').contains('vite')
+  })
+
+
+  it('button', () => {
+    cy.wait('@getCountInterval').then((interception) => {
+      const increment = interception.response.body
+
+      cy.contains('count is 0')
+      cy.get('button').click()
+      cy.contains(`count is ${increment}`)
+      cy.get('button').click()
+      cy.contains(`count is ${2 * increment}`)
+    })
   })
 })
